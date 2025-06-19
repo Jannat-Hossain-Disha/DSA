c577503565e1bf93cd246d1357e3c3010fb31110
Jannat Hossain Disha <jannathossaindisha@gmail.com>
2025-06-20 00:17:23 +0600

    printing-graph

---
+++ Graph/1-undirectedGraphAdjacencyMatrix.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Enter the number of Vertices : ";
+    int n,e;
+    cin>>n;
+    cout<<"Enter the number of Edges : ";
+    cin>>e;
+    vector<vector<int>> adj(n, vector<int>(n, 0));
+    int edges[e][2];
+    cout<<"Enter the Edges : ";
+    for(int i=0;i<e;i++)
+    {
+        for(int j=0;j<2;j++)
+        cin>>edges[i][j];
+    }
+    for(int i=0;i<e;i++)
+    {
+        int u=edges[i][0];
+        int v=edges[i][1];
+        adj[u][v]=1;
+        adj[v][u]=1; //for undirected graph
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+            cout<<adj[i][j]<<" ";
+        cout<<endl;
+    }
+
+}
