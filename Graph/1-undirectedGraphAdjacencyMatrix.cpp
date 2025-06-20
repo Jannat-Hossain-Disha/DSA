38bb26206ba2bdfde39a2f94d8f7cd57758f445a
Jannat Hossain Disha <jannathossaindisha@gmail.com>
2025-06-20 11:48:26 +0600

    adjacency list added

--- Graph/1-undirectedGraphAdjacencyMatrix.cpp
+++ Graph/1-undirectedGraphAdjacencyMatrix.cpp
@@ -1,32 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    cout<<"Enter the number of Vertices : ";
-    int n,e;
-    cin>>n;
-    cout<<"Enter the number of Edges : ";
-    cin>>e;
+    cout << "Enter the number of Vertices : ";
+    int n, e;
+    cin >> n;
+    cout << "Enter the number of Edges : ";
+    cin >> e;
     vector<vector<int>> adj(n, vector<int>(n, 0));
     int edges[e][2];
-    cout<<"Enter the Edges : ";
-    for(int i=0;i<e;i++)
+    cout << "Enter the Edges : ";
+    for (int i = 0; i < e; i++)
     {
-        for(int j=0;j<2;j++)
-        cin>>edges[i][j];
+        for (int j = 0; j < 2; j++)
+            cin >> edges[i][j];
     }
-    for(int i=0;i<e;i++)
+    for (int i = 0; i < e; i++)
     {
-        int u=edges[i][0];
-        int v=edges[i][1];
-        adj[u][v]=1;
-        adj[v][u]=1; //for undirected graph
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u][v] = 1;
+        adj[v][u] = 1; // for undirected graph
     }
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
-            cout<<adj[i][j]<<" ";
-        cout<<endl;
+        for (int j = 0; j < n; j++)
+            cout << adj[i][j] << " ";
+        cout << endl;
     }
-
 }
---
+++ Graph/3-undirectedGraphAdjacencyList.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, e;
+    cout << "Enter the value of vertices : ";
+    cin >> n;
+    cout << "Enter the value of edges : ";
+    cin >> e;
+    vector<vector<int>> adj(n);
+    cout << "Enter the edges : ";
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> ";
+        for (auto u : adj[i])
+            cout << u << " ";
+        cout << endl;
+    }
+}
